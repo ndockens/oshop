617bff34c597756cc18d11bcfa5036e8197ce966
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2019-07-12 21:03:35 -0500

    Got firebase deployment working.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { ProductsComponent } from "./components/products/products.component";
 import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
+import { MyOrdersComponent } from "./components/my-orders/my-orders.component";
 import { CheckoutComponent } from "./components/checkout/checkout.component";
 import { OrderSuccessComponent } from "./components/order-success/order-success.component";
 import { LoginComponent } from "./components/login/login.component";
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "products", component: ProductsComponent },
   { path: "shopping-cart", component: ShoppingCartComponent },
+  { path: "my/orders", component: MyOrdersComponent },
   { path: "checkout", component: CheckoutComponent },
   { path: "order-success", component: OrderSuccessComponent },
   { path: "login", component: LoginComponent },
