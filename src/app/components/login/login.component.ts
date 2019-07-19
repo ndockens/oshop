bfeebe44323adff01712bb19fe39670aefd089dc
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2019-07-19 16:22:03 -0500

    Enabled Google login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,7 +11,6 @@ export class LoginComponent {
   constructor(private afAuth: AngularFireAuth) {}
 
   login() {
-    //this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
-    console.log("login");
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 }
